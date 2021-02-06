6679ddb5ee3ad78a5869d7b1e7223ec139d23887
Auden Robertson <audenrobertson@gmail.com>
2021-02-05 22:43:24 -0500

    created stateless component

--- typescript-project/src/App.tsx
+++ typescript-project/src/App.tsx
@@ -2,24 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Message from './Message';
+
 class App extends Component {
   render() {
-    //tuple
-    let aTuple: [string, number] = ['Auden', 44]
-
-    //enum
-    enum Codes {first = 1, second= 2};
-
-    //any
-    let firstName: any = 'Earlin'
+   
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            The value {firstName} is of { typeof firstName} type!
-          </p>       
+         <Message />      
         </header>
       </div>
     );
